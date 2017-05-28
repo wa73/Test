19e6a48a841228ed05e787ebad07875b9c92fbfb
wa73 <shailu.sangepu@gmail.com>
2017-05-28 11:00:29 +0000

    Frist commit

---
+++ src/Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Hello{
+ 
+  public static void main(String args[]){
+ 
+    /*
+    Use System.out.println() to print on console.
+    */
+    System.out.println("Hello World !");
+    System.out.println("Hello World2 !");
+    System.out.println("Hello World3 !"); 
+  }
+ 
+}
